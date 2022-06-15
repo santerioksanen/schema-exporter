95ab95c58417c8ee75eaf11c681519815d51deb5
Santeri Oksanen <santeri.oksanen@lumine.fi>
2022-06-15 11:05:00 +0300

    Change version to more appropriate early version, add info to setup

--- setup.py
+++ setup.py
@@ -2,7 +2,10 @@ from setuptools import setup
 
 setup(
     name='marshmallow-export',
-    version='1.0.0',
+    version='0.1.0',
+    description='Export Marshmallow schemas to Rust and Typescript',
+    author='Santeri Oksanen',
+    url='https://github.com/santerioksanen/marshmallow-export',
     packages=['marshmallow_export'],
     install_requires=['marshmallow', 'marshmallow_enum'],
 )
