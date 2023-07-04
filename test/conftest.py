f8b138768b9fe9d33df77b718e441f63db6315ff
santeriok <santeri@montel.fi>
2023-07-04 11:24:12 +0300

    Add django conf for models to function

---
+++ test/conftest.py
@@ -0,0 +1,23 @@
+import django
+
+
+def pytest_configure():
+    from django.conf import settings
+
+    settings.configure(
+        DEBUG_PROPAGATE_EXCEPTIONS=True,
+        DATABASES={
+            "default": {
+                "Engine": "django.db.backends.sqlite3",
+                "NAME": ":memory:",
+            }
+        },
+        MIDDLEWARE=(
+            "django.middleware.common.CommonMiddleware",
+        ),
+        INSTALLED_APPS=(
+            "rest_framework",
+        )
+    )
+
+    django.setup()
