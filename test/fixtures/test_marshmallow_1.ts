93c9c82b555c7903639ee613deae4ebdae3d5bc0
santeriok <santeri@montel.fi>
2023-07-05 10:47:32 +0300

    Export ts dates as strings, strip semicolons from ts export

--- test/fixtures/test_marshmallow_1.ts
+++ test/fixtures/test_marshmallow_1.ts
@@ -11,32 +11,32 @@ export enum TestEnum2 {
 }
 
 export interface Leaf {
-  bool_1?: boolean;
-  boolean_1?: boolean;
-  datetime_1?: Date;
-  decimal_1?: number;
-  int_1?: number;
-  integer_1?: number;
+  bool_1?: boolean
+  boolean_1?: boolean
+  datetime_1?: string
+  decimal_1?: number
+  int_1?: number
+  integer_1?: number
 }
 
 export interface Leaf2 {
-  datetime_1: Date;
-  decimal_1: number;
+  datetime_1: string
+  decimal_1: number
 }
 
 export interface Middle {
-  test_enum_2?: TestEnum2[];
-  leaf_schema?: Leaf;
+  test_enum_2?: TestEnum2[]
+  leaf_schema?: Leaf
 }
 
 export interface Root2 {
-  nested_leaf_1: Leaf2;
+  nested_leaf_1: Leaf2
 }
 
 export interface Root {
-  nested_leaf_1?: Middle;
-  nested_leaf_2?: Middle[];
-  list_leaf_1?: Middle[];
-  test_enum_1?: TestEnum1;
-  test_enum_1_marshmallow?: TestEnum1;
+  nested_leaf_1?: Middle
+  nested_leaf_2?: Middle[]
+  list_leaf_1?: Middle[]
+  test_enum_1?: TestEnum1
+  test_enum_1_marshmallow?: TestEnum1
 }
